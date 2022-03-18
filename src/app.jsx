2aabb4656addfc329c0911cf3fd077d46af13add
Chanwoo <tree698@gmail.com>
2022-03-18 18:52:15 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,28 @@
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './component/video_list/video_list';
+import items from './videoList.json';
 
 function App() {
-  return <h1>Welcome to My YouTube! 👩‍🦳</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow',
+    // };
+
+    // fetch(
+    //   'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=AIzaSyD_DLuVderXOEVvn1b99umo8c-UbWpVClk',
+    //   requestOptions
+    // )
+    //   .then((response) => response.json())
+    //   .then((result) => setVideos(result.items))
+    //   .catch((error) => console.log('error', error));
+
+    setVideos(items.items);
+  });
+  return <VideoList videos={videos} />;
 }
 
 export default App;
---
+++ src/component/video_item/video_item.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+export default VideoItem;
